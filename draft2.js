1a82662ca90c5f41cb6918e6b9fbcb9b17271c0f
Benj Chun <67034610+BenjaminChun@users.noreply.github.com>
2021-05-25 09:17:04 +0800

    Update draft2.js

--- draft2.js
+++ draft2.js
@@ -1,1 +1,11 @@
-function 
+const portfolioitems = document.querySelectorAll('.portfolio-item-wrapper');
+portfolioitems.forEach(portfolioitem =>{
+	portfolioitem.addEventListener('mouseover',()=>{
+		console.log(portfolioitem.childNodes[1].classList);
+		portfolioitem.childNodes[1].classList.add('img-darken');
+	})
+	portfolioitem.addEventListener('mouseout',()=>{
+		console.log(portfolioitem.childNodes[1].classList);
+		portfolioitem.childNodes[1].classList.remove('img-darken');
+	})
+})
